a8c33707bfe3b66103d89741257decc782cbe4e8
Frqcture <joedavidmoore.jm@gmail.com>
2022-05-03 16:09:22 +0100

    Started to build summary Tab Widget

--- lib/summary_tab.dart
+++ lib/summary_tab.dart
@@ -10,6 +10,28 @@ class SummaryTab extends StatefulWidget {
 class _SummaryTabState extends State<SummaryTab> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+      child: Column(children: <Widget>[
+        Container(
+          child: Row(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerLeft,
+                child: const Icon(
+                  Icons.abc
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: const  Text(
+                  'TITLE',
+                  style: TextStyle(fontSize: 20,),),
+              ),
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
