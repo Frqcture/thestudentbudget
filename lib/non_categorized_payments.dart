c329446ed373edfee0afcc050c2695d4d136d7c7
Frqcture <joedavidmoore.jm@gmail.com>
2022-05-06 11:36:52 +0100

    Fixed new tab, can see on screen

--- lib/non_categorized_payments.dart
+++ lib/non_categorized_payments.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'payment_info_summaries.dart';
 
 class NonCategorisedPaymentScreen extends StatefulWidget {
   const NonCategorisedPaymentScreen({Key? key}) : super(key: key);
@@ -7,11 +8,38 @@ class NonCategorisedPaymentScreen extends StatefulWidget {
   State<NonCategorisedPaymentScreen> createState() => _NonCategorisedPaymentScreenState();
 }
 
-class _NonCategorisedPaymentScreenState extends State<NonCategorisedPaymentScreen> {
+class _NonCategorisedPaymentScreenState extends State<NonCategorisedPaymentScreen> {  
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hello, world'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TheStudentBudget'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(15, 25, 15, 25),
+        child: Column(children: <Widget>[
+          const Text(
+            'Payments to categorize:',
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              border: Border.all(color: const Color.fromARGB(255, 224, 224, 224))
+            ),
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(15, 0, 15, 10),
+                child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  NoCategoryPaymentInfo(paidTo: 'Joe', amount: 123.45,),
+                  NoCategoryPaymentInfo(paidTo: 'Joe', amount: 123.45,),
+                ],
+              ),
+            )
+          )
+        ],)
+      )
     );
   }
 }
--- lib/payment_info_summaries.dart
+++ lib/payment_info_summaries.dart
@@ -41,3 +41,39 @@ class _PaymentInfoSummaryState extends State<PaymentInfoSummary> {
           );
   }
 }
+
+class NoCategoryPaymentInfo extends StatefulWidget {
+  NoCategoryPaymentInfo({Key? key, required this.paidTo, required this.amount}) : super(key: key);
+
+  final String paidTo;
+  final double amount;
+
+  @override
+  State<NoCategoryPaymentInfo> createState() => _NoCategoryPaymentInfoState();
+}
+
+class _NoCategoryPaymentInfoState extends State<NoCategoryPaymentInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+        Container(
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(widget.paidTo),
+              Text('£'+widget.amount.toString())
+            ],
+          )
+        ),
+        Container(
+          alignment: Alignment.bottomLeft,
+          child: Icon(
+            Icons.abc
+          ),
+        )
+      ],)
+    );
+  }
+}
