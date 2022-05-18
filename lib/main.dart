75803f176baef1cc9ecd455fea09f29e29ef1ab8
Joe Moore <98429175+Frqcture@users.noreply.github.com>
2022-05-18 12:32:08 +0100

    Added firebase and initialized, no other changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'HomeScreenWidgets/home_screen.dart';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   runApp(const MyApp());
 }
 
